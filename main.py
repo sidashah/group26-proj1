178f3864a180e1ab56129b5d4c9ce1ea3f0faa9d
Siddharth Shah <sas2387@columbia.edu>
2017-09-21 16:40:53 -0400

    Basic flow of the program for first iteration

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
 from get_json_from_url import get_json_from_url
 
 if __name__ == '__main__':
-	print get_json_from_url('http://siddharthshah.in/jaguar.txt')
+	query = raw_input("Please enter your query: ")
+	precision_required = float(raw_input("Precision required:"))
 
+	# First Iteration
+	# use query, engine id and API Key to make call on Google Custom Search
+	json_data = get_json_from_url('http://siddharthshah.in/jaguar.txt')
+	search_count = json_data['queries']['request'][0]['count']
+	if search_count != 10 :
+		# end program over here
+		pass
+	result_feedback = [False] * search_count
+	# display results to user and get feedback
+	i = 0
+	precision = 0
+	for item in json_data['items']:
+		print('\nTitle: ' + item['title'])
+		print('Snippet: '+ item['snippet'])
+		user_feedback = raw_input("Is this relevant?(Y/N): ")
+		while user_feedback not in ['Y','y','N','n']:
+			user_feedback = raw_input("Please answer in terms of 'Y' or 'N': ")
+		if user_feedback in ['Y','y']:
+			result_feedback[i] = True
+			precision += 1
+		i += 1
+
+	if precision / search_count > precision_required:
+		# done - end the program
+		pass
+
+	relevant_results = []
+	i = 0
+	for item in json_data['items']:
+		if result_feedback[i]:
+			relevant_results.append(item['title'] + ' ' + item['snippet'])
+		i += 1
+
+	print relevant_results
