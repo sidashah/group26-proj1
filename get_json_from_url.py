60f3dde87c300e3dc32149e9dd9bd43a1a53cdb8
Akshay nagpal <akshay2626@gmail.com>
2017-09-21 21:42:24 -0400

    query_string build from parameters dict

--- get_json_from_url.py
+++ get_json_from_url.py
@@ -1,9 +1,11 @@
 import urllib, json
 
-def get_json_from_url(url):
+def get_json_from_url(url, parameters):
     """
     :rtype: string
     """
-    response = urllib.urlopen(url)
+    query_string = urllib.urlencode(parameters)
+    final_url = url+'?'+query_string
+    response = urllib.urlopen(final_url)
     data = json.loads(response.read())
     return data
